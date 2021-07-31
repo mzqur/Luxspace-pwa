825054076e5b361a2862f5b08925cd1fa688888d
mzqur <scoutquris@gmail.com>
2021-07-31 18:33:33 +0700

    make component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './styles/tailwind.output.css';
+import Header from './component/Header';
+import Hero from './component/Hero';
+import Browse from './component/Browse';
+import Arived from './component/Arived';
+import Clients from './component/Clients';
+import AsideMenu from './component/AsideMenu';
+import Footer from './component/Footer';
 
 function App() {
   return (
-    <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg ">
-        <div className="ml-6 pt-1">
-            <h1 className="text-2xl text-blue-700 leading-tight">
-              Tailwind
-            </h1>
-        </div>
-    </div>
+    <div>
+      <Header /> 
+      <Hero />
+      <Browse />
+      <Arived />
+      <Clients />
+      <AsideMenu />
+      <Footer />
+    </div> 
   );
 }
 
