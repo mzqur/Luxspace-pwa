b88faddb78aa72bd7fb9eeb17edcf4ca49d08ffd
mzqur <scoutquris@gmail.com>
2021-07-31 16:41:27 +0700

    instalation& config  Tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/tailwind.output.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg ">
+        <div className="ml-6 pt-1">
+            <h1 className="text-2xl text-blue-700 leading-tight">
+              Tailwind
+            </h1>
+        </div>
     </div>
   );
 }
