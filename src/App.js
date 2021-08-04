bf6aa4107a2f44442b5ca7c6e390cff57862b6af
mzqur <scoutquris@gmail.com>
2021-08-04 11:58:54 +0700

    manage component directory

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './styles/tailwind.output.css';
-import Header from './component/Header';
-import Hero from './component/Hero';
-import Browse from './component/Browse';
-import Arived from './component/Arived';
-import Clients from './component/Clients';
-import AsideMenu from './component/AsideMenu';
-import Footer from './component/Footer';
+import Header from './component/header/Header';
+import Hero from './component/hero/Hero';
+import Browse from './component/browse/Browse';
+import Arived from './component/arived/Arived';
+import Clients from './component/client/Clients';
+import AsideMenu from './component/aside_menu/AsideMenu';
+import Footer from './component/footer/Footer';
 
 function App() {
   return (
