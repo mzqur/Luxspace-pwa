4a992e9ee0ceb10cf749fbbbe03c44d64fa32e9a
mqbr <muhquris@gmail.com>
2021-08-09 15:00:27 +0700

    undo changing font

--- tailwind.config.js
+++ tailwind.config.js
@@ -277,7 +277,20 @@ module.exports = {
     },
     fontFamily: {
       sans: [
-       '"Josefin Sans"'
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Roboto",
+        '"Helvetica Neue"',
+        "Arial",
+        '"Noto Sans"',
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
       ],
       serif: [
         "ui-serif",
